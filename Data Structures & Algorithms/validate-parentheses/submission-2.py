bf1fb763683e2c19b1ab7567077ec05c28f1f8f1
Fiodar Hancharou <60438923+fiodarhancharou@users.noreply.github.com>
2026-08-29 12:11:42 +0200

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        br_map = {")": "(", "}": "{", "]": "["}
+        stack = []
+        for br in s:
+            if br in br_map:
+                if (not stack or br_map[br] != stack.pop()):
+                    return False
+            else:
+                stack.append(br)
+        is_empty = not bool(stack)
+        return is_empty
