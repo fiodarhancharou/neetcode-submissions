5fc0c4b67059e5643c787ecdd90d5019602d0dbb
Fiodar Hancharou <60438923+fiodarhancharou@users.noreply.github.com>
2026-08-29 12:57:08 +0200

    Add: implement-stack-using-queues - submission-0

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-0.py
@@ -0,0 +1,32 @@
+from queue import Queue
+class MyStack:
+
+    def __init__(self):
+        self.queue1 = Queue()
+        self.queue2 = Queue()
+    def push(self, x: int) -> None:
+        self.queue1.put(x)
+
+    def pop(self) -> int:
+        while self.queue1.qsize() > 1:
+            self.queue2.put(self.queue1.get())
+        res = self.queue1.get()
+        self.queue1 = self.queue2
+        self.queue2 = Queue()
+        return res
+
+    def top(self) -> int:
+        res = self.pop()
+        self.queue1.put(res)
+        return res
+
+    def empty(self) -> bool:
+        return self.queue1.empty()  
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
